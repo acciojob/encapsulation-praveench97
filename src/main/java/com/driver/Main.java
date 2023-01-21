2fd91c84f0ab29456627661a117d3b9f93f55732
praveench97 <praveench1997@gmail.com>
2023-01-21 13:43:46 +0530

    Trail encapsulation 1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String[] args){
+      RWOnly rw = new RWOnly();
+//      rw.name = "newName";
+//      System.out.println(rw.name);
+      //name has private access in com.driver.RWOnly
+
+      rw.setName("newName");
+      System.out.println(rw.getName());
+    }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String name;
+    void setName(String name){
+        this.name = name;
+    }
+    String getName(){
+        return this.name;
+    }
 }
